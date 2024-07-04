5dbc6ead41a10a0807407b1629fc482ce6767e21
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-04 15:28:36 -0400

    css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link'
+import NavbarMain from '@/components/navbar-main';
 
 const Home: NextPage = () => {
   return (
@@ -8,39 +8,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Cello Repertoire Initiative</title>
       </Head>
-      <header className="bg-gray-100 text-black p-4 flex items-center justify-between">
-        <div className="flex items-center">
-          <div className="flex-shrink-0">
-            <img src="/assets/altLogo.png" alt="Logo" className="h-16 w-auto" />
-          </div>
-          <span className="ml-8 text-xl font-bold bg-clip-text text-transparent bg-gradient-to-br">Cello Repertoire Initiative</span>
-
-        </div>
-        <div className="flex-grow max-w-lg mx-4">
-          <input
-            type="text"
-            placeholder="Find music, composers, and resources..."
-            className="w-full p-2 rounded bg-gray-300 border border-gray-600 text-white"
-          />
-        </div>
-        <nav>
-          <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-            Home
-          </button>
-          <Link href="/music">
-          <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-            Music
-          </button>
-          </Link>
-          <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-            Contact
-          </button>
-        </nav>
-      </header>
+      <NavbarMain />
       <main className="p-4 flex flex-col items-center justify-center h-screen">
-  <h1 className="text-2xl font-bold mb-4">Welcome to the Cello Repertoire Initiative!</h1>
-  <p className="text-center">Your source for curated cello music pieces.</p>
-</main>
+        <h1 className="text-2xl font-bold mb-4">Welcome to the Cello Repertoire Initiative!</h1>
+        <p className="text-center">Your source for curated cello music pieces.</p>
+
+        <div className="ml-auto mt-8 text-right">
+          <div className="routerLinkToPieces block mb-2">Find music for your students</div>
+          <br />
+          <div className="routerLinkToPieces block mb-2">Browse our catalogue</div>
+          <br />
+          <div className="routerLinkToPieces block mb-2">Discover composers</div>
+        </div>
+      </main>
     </div>
   );
 };
