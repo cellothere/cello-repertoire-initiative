617e6d9119af9bed9efc2d4368cef4b3a6f21c6c
cellothere <109959547+cellothere@users.noreply.github.com>
2024-07-04 20:01:12 -0400

    adding dropdown

--- components/navbar-main.tsx
+++ components/navbar-main.tsx
@@ -1,41 +1,95 @@
-//main nav bar
 import Link from 'next/link';
-import Head from 'next/head';
+import { useState } from 'react';
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/dropdown';
 
 const NavbarMain = () => {
+  // State to track hover state
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <header className="bg-gray-100 text-black p-4 flex items-center justify-between">
-    <div className="flex items-center">
-      <div className="flex-shrink-0">
-        <img src="/assets/altLogo.png" alt="Logo" className="h-16 w-auto" />
+      <div className="flex items-center">
+        <div className="flex-shrink-0">
+          <img src="/assets/altLogo.png" alt="Logo" className="h-16 w-auto" />
+        </div>
+        <span className="ml-8 text-xl font-bold bg-clip-text text-transparent bg-gradient-to-br">Cello Repertoire Initiative</span>
       </div>
-      <span className="ml-8 text-xl font-bold bg-clip-text text-transparent bg-gradient-to-br">Cello Repertoire Initiative</span>
+      <div className="flex-grow max-w-lg mx-4">
+        <input
+          type="text"
+          placeholder="Find music, composers, and resources..."
+          className="w-full p-2 rounded bg-gray-300 border border-gray-600 text-white"
+        />
+      </div>
+      <nav>
+        <Link href="/">
+          <button className="bg-purple-500 hover:bg-teal-400 text-white font-bold py-2 px-4 rounded mx-2">
+            Home
+          </button>
+        </Link>
+        <Dropdown
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          isOpen={isHovered}
+        >
+          <DropdownTrigger>
+            <button
+              className="bg-purple-500 text-white font-bold py-2 px-4 rounded mx-2"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <Link href="/music">
+                Music
+              </Link>
+            </button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Music Options"
+            className="custom-dropdown-menu"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <DropdownItem className="custom-dropdown-item">
+              <Link href="#">
+                Violin Sheet Music
+              </Link>
+            </DropdownItem>
+            <DropdownItem className="custom-dropdown-item">
+              <Link href="#">
+                Viola Sheet Music
+              </Link>
+            </DropdownItem>
+            <DropdownItem className="custom-dropdown-item">
+              <Link href="#">
+                Cello Sheet Music
+              </Link>
+            </DropdownItem>
+            <DropdownItem className="custom-dropdown-item">
+              <Link href="#">
+                Bass Sheet Music
+              </Link>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Link href="/contact">
+          <button className="bg-purple-500 hover:bg-teal-400 text-white font-bold py-2 px-4 rounded mx-2">
+            Contact
+          </button>
+        </Link>
+      </nav>
+      <style jsx>{`
+        .custom-dropdown-menu {
+          background-color: white;
+        }
+        .custom-dropdown-item {
+          margin-bottom: 8px; /* Adjust the value as needed */
+        }
+        .custom-dropdown-item:last-child {
+          margin-bottom: 0;
+        }
+      `}</style>
+    </header>
+  );
+};
 
-    </div>
-    <div className="flex-grow max-w-lg mx-4">
-      <input
-        type="text"
-        placeholder="Find music, composers, and resources..."
-        className="w-full p-2 rounded bg-gray-300 border border-gray-600 text-white"
-      />
-    </div>
-    <nav>
-    <Link href="/">
-      <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-        Home
-      </button>
-    </Link>
-      <Link href="/music">
-      <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-        Music
-      </button>
-      </Link>
-      <button className="bg-purple-500 hover:bg-teal-400  text-white font-bold py-2 px-4 rounded mx-2">
-        Contact
-      </button>
-    </nav>
-  </header>
-  )
-}
-
-export default NavbarMain
+export default NavbarMain;
---
+++ pages/api/composers.ts
@@ -0,0 +1,52 @@
+// pages/api/composers.ts
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../lib/mongodb';
+
+// Define the Composer type
+type Composer = {
+  composer_name: string;
+  composer_bio: string;
+  wikipedia_article: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const client = await clientPromise;
+    const collection = client.db('cello_repertoire').collection('composers');
+
+    const composers = await collection
+      .aggregate<Composer>([
+        {
+          $sort: {
+            composer_name: 1,
+          },
+        },
+        {
+          $group: {
+            _id: {
+              $toLower: { $substrCP: ['$composer_name', 0, 1] },
+            },
+            composers: {
+              $push: {
+                composer_name: '$composer_name',
+                composer_bio: '$composer_bio',
+                wikipedia_article: '$wikipedia_article',
+              },
+            },
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+      ])
+      .toArray();
+
+    res.status(200).json(composers);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch composers' });
+  }
+}
