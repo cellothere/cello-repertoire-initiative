40405adc0741c8f3f582e554b58bd41244bd67f3
cellothere <109959547+cellothere@users.noreply.github.com>
2024-08-17 13:07:48 -0400

    Changed "violin" page into "see what's new page"

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,11 +45,12 @@ const Home: NextPage = () => {
         </button>
         <div className={`mt-8 ${contentVisible ? '' : 'hidden'}`} id="afterMoveUp">
           <div className={`flex ${isMobileView ? 'flex-col space-y-6' : 'space-x-6'}`} id="images-container">
+            
             <Link href="/cello-music">
               <div className="relative hover:scale-105 duration-300">
-                <img src="/assets/violin2.png" className="w-80 h-120 rounded-lg hover:opacity-75 transition-opacity duration-300" />
+                <img src="/assets/whatsNew.png" className="w-80 h-120 rounded-lg hover:opacity-75 transition-opacity duration-300" />
                 <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
-                  <p className="text-white text-lg font-bold">Violin Music</p>
+                  <p className="text-white text-lg font-bold">See What's New</p>
                   <p className="text-sm font-bold">Coming Soon</p>
                 </div>
               </div>
