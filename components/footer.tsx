3b252b78c37cebcf65c1a8e5687ff0d56028b25b
cellothere <109959547+cellothere@users.noreply.github.com>
2025-03-14 15:32:56 -0400

    Footer wrap on mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full text-center text-xs font-mono py-4 text-white shadow-md whitespace-nowrap overflow-hidden text-ellipsis">
+    <footer className="w-full text-center text-xs font-mono py-4 text-white shadow-md whitespace-normal sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
       © {new Date().getFullYear()} Cello Repertoire Initiative - All Rights Reserved
     </footer>
   );
