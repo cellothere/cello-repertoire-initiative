b4229fb2462d31ce09d6892920d0e0daa02095b0
cellothere <109959547+cellothere@users.noreply.github.com>
2025-03-14 10:45:50 -0400

    Adding a footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="w-full text-center text-sm font-mono py-4 text-white shadow-md">
+        © {new Date().getFullYear()} Cello Repertoire Initiative - All Rights Reserved
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react"
+import Footer from "@/components/footer"; // Adjust the import path as needed
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    
-    <div className="bg-gradient-to-br from-purple-500 to-teal-400 min-h-screen">
-      <Component {...pageProps} />
+    <div className="bg-gradient-to-br from-purple-500 to-teal-400 min-h-screen flex flex-col">
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
       <Analytics />
     </div>
   );
