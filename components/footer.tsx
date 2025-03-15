895bf5cb49bdb44db2f9c25da9f998820d18dbbd
cellothere <109959547+cellothere@users.noreply.github.com>
2025-03-15 16:04:06 -0400

    Adding a footer. Needs more sytling!

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,25 @@
+import { FaDatabase, FaStar, FaExternalLinkAlt } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <footer className="w-full text-center text-xs font-mono py-4 text-white shadow-md whitespace-normal sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
-      © {new Date().getFullYear()} Cello Repertoire Initiative - All Rights Reserved
+    <footer className="w-full text-white border-t border-white/50 shadow-sm">
+      <div className="flex flex-row items-center justify-center p-4">
+        <button className="flex flex-col items-center my-2 mx-2 lg:mx-12 hover:opacity-80">
+          <FaDatabase size={24} />
+          <span className="mt-1 text-xs">Composer Database</span>
+        </button>
+        <button className="flex flex-col items-center my-2 mx-2 lg:mx-12 hover:opacity-80">
+          <FaStar size={24} />
+          <span className="mt-1 text-xs">Featured Databases</span>
+        </button>
+        <button className="flex flex-col items-center my-2 mx-2 lg:mx-12 hover:opacity-80">
+          <FaExternalLinkAlt size={24} />
+          <span className="mt-1 text-xs">External Resources</span>
+        </button>
+      </div>
+      <div className="text-center text-xs font-mono py-2">
+        © {new Date().getFullYear()} Cello Repertoire Initiative - All Rights Reserved
+      </div>
     </footer>
   );
 };
