acf46b10a4e7676f9015f3fd9c1c4e20b5b335ec
cellothere <109959547+cellothere@users.noreply.github.com>
2025-03-16 14:48:16 -0400

    Optimizing

--- components/music-card.tsx
+++ components/music-card.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { CiHeart } from 'react-icons/ci';
 import { FaInfoCircle, FaSpinner } from 'react-icons/fa';
 
 interface MusicCardProps {
